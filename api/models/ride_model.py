5e02a831b4aa0867793b0c7412f639c03ed3678e
Sufiyaan Khan <117508034+Mrbunny159@users.noreply.github.com>
2025-11-10 00:46:41 +0530

    Update ride_model.py

--- api/models/ride_model.py
+++ api/models/ride_model.py
@@ -1,1 +1,17 @@
+from utils.db import db
 
+class RideModel:
+    @staticmethod
+    def get_all_rides():
+        # Return all rides from NeoDB
+        pass
+
+    @staticmethod
+    def create_ride(organizer_id, data):
+        # Insert ride doc
+        pass
+
+    @staticmethod
+    def join_ride(ride_id, user_id):
+        # Add user to ride participants
+        pass
