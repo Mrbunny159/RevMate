a0917cd02cc53dd66c5bfe3af2a91531fff9c413
Sufiyaan Khan <117508034+Mrbunny159@users.noreply.github.com>
2025-11-10 00:46:55 +0530

    Update db.py

--- api/utils/db.py
+++ api/utils/db.py
@@ -1,1 +1,22 @@
+# Example placeholder, will need Vercel NeoDB connector and setup
+class db:
+    @staticmethod
+    def insert(collection, document):
+        # Insert document
+        pass
+
+    @staticmethod
+    def find(collection, query):
+        # Find documents
+        pass
+
+    @staticmethod
+    def update(collection, query, data):
+        # Update document
+        pass
+
+    @staticmethod
+    def delete(collection, query):
+        # Delete doc
+        pass
 
