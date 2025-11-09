b16d3ff231a76525ae05b2d41389d92770c99d7a
Sufiyaan Khan <117508034+Mrbunny159@users.noreply.github.com>
2025-11-10 00:46:26 +0530

    Update user_model.py

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,1 +1,33 @@
+from utils.db import db
+
+class UserModel:
+    @staticmethod
+    def create_user(data):
+        # Hash password, insert user doc to NeoDB users collection
+        pass
+
+    @staticmethod
+    def get_user(user_id):
+        # Return user by ID
+        pass
+
+    @staticmethod
+    def verify_credentials(email, password):
+        # Fetch user, check hash
+        pass
+
+    @staticmethod
+    def update_user(user_id, data):
+        # Update fields
+        pass
+
+    @staticmethod
+    def delete_user(user_id):
+        # Remove user doc
+        pass
+
+    @staticmethod
+    def is_email_available(email):
+        # Query users
+        pass
 
